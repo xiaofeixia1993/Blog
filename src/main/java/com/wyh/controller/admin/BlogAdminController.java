8af406e57aa7c439b08d4cc5b4accf3dffeeff32
master <11221324@qq.com>
2019-12-28 18:03:31 +0800

    博客信息后台管理界面实现、博客信息分页查询实现

--- src/main/java/com/wyh/controller/admin/BlogAdminController.java
+++ src/main/java/com/wyh/controller/admin/BlogAdminController.java
@@ -1,15 +1,23 @@
 package com.wyh.controller.admin;
 
 import com.wyh.entity.Blog;
+import com.wyh.entity.PageBean;
 import com.wyh.lucene.BlogIndex;
 import com.wyh.service.BlogService;
 import com.wyh.util.ResponseUtil;
+import com.wyh.util.StringUtil;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 管理员博客Controller层
@@ -47,6 +55,34 @@ public class BlogAdminController {
         }else{
             result.put("success", false);
         }
+        ResponseUtil.write(response, result);
+        return null;
+    }
+
+    /**
+     * 分页查询博客信息
+     * @param page
+     * @param rows
+     * @param s_blog
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/list")
+    public String list(@RequestParam(value="page",required=false)String page, @RequestParam(value="rows",required=false)String rows, Blog s_blog, HttpServletResponse response)throws Exception{
+        PageBean pageBean=new PageBean(Integer.parseInt(page),Integer.parseInt(rows));
+        Map<String,Object> map=new HashMap<String,Object>();
+        map.put("title", StringUtil.formatLike(s_blog.getTitle()));
+        map.put("start", pageBean.getStart());
+        map.put("size", pageBean.getPageSize());
+        List<Blog> blogList=blogService.list(map);
+        Long total=blogService.getTotal(map);
+        JSONObject result=new JSONObject();
+        JsonConfig jsonConfig=new JsonConfig();
+        jsonConfig.registerJsonValueProcessor(java.util.Date.class, new DateJsonValueProcessor("yyyy-MM-dd"));
+        JSONArray jsonArray=JSONArray.fromObject(blogList, jsonConfig);
+        result.put("rows", jsonArray);
+        result.put("total", total);
         ResponseUtil.write(response, result);
         return null;
     }
