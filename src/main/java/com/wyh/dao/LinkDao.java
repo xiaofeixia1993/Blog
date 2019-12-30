6746ab8f3610e0f57cca915c565c76e7ee5a21d0
master <11221324@qq.com>
2019-12-30 22:58:23 +0800

    分页查询友情链接功能实现、友情链接增删改

--- src/main/java/com/wyh/dao/LinkDao.java
+++ src/main/java/com/wyh/dao/LinkDao.java
@@ -18,4 +18,32 @@ public interface LinkDao {
      * @return
      */
     public List<Link> list(Map<String,Object> map);
+
+    /**
+     * 获取总记录数
+     * @param map
+     * @return
+     */
+    public Long getTotal(Map<String,Object> map);
+
+    /**
+     * 添加友情链接信息
+     * @param link
+     * @return
+     */
+    public Integer add(Link link);
+
+    /**
+     * 修改友情链接信息
+     * @param link
+     * @return
+     */
+    public Integer update(Link link);
+
+    /**
+     * 删除友情链接信息
+     * @param id
+     * @return
+     */
+    public Integer delete(Integer id);
 }
