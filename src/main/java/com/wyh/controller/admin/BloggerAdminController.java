05ae370375f8b41029d9eb612e0a8c4be2e6cb8a
master <11221324@qq.com>
2019-12-30 21:21:45 +0800

    个人信息展示

--- src/main/java/com/wyh/controller/admin/BloggerAdminController.java
+++ src/main/java/com/wyh/controller/admin/BloggerAdminController.java
@@ -1,10 +1,14 @@
 package com.wyh.controller.admin;
 
+import com.wyh.entity.Blogger;
 import com.wyh.service.BloggerService;
+import com.wyh.util.ResponseUtil;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 管理员博主Controller层
@@ -17,4 +21,18 @@ public class BloggerAdminController {
 
     @Resource
     private BloggerService bloggerService;
+
+    /**
+     * 查询博主信息
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/find")
+    public String find(HttpServletResponse response)throws Exception{
+        Blogger blogger=bloggerService.find();
+        JSONObject jsonObject=JSONObject.fromObject(blogger);
+        ResponseUtil.write(response, jsonObject);
+        return null;
+    }
 }
